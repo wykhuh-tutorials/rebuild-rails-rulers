c72ffdd257ead7295f2a42c31fd2666cfcf65542
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-08-16 23:25:01 -0700

    1 move "hello" response to gem

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,5 +1,14 @@
 require "rulers/version"
 
 module Rulers
-  # Your code goes here...
+  class Application
+    def call(env)
+      `echo debug > debug.txt`;
+      [
+        200,
+        {'Content-Type' => 'text/html'},
+        ["Hello from Ruby on Rulers!"]
+      ]
+    end
+  end
 end
