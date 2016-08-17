ab61e0c9c8944f05b9447f6ddc2a793196909abe
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-08-17 07:46:43 -0700

    2 super hacky way to deal with favicon

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,6 +4,15 @@ require "rulers/routing"
 module Rulers
   class Application
     def call(env)
+      # hacky way to deal with favicon
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [
+          404,
+          {'Content-Type' => 'text/html'},
+          []
+        ]
+      end
+
       # get controller and action from the url
       klass, act = get_controller_and_action(env)
       # make new controller
