83cb3326bfe38eea82f96a43fd8e70b562764a94
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-08-18 00:02:56 -0700

    3 autoload controllers

--- lib/rulers/util.rb
+++ lib/rulers/util.rb
@@ -7,6 +7,7 @@ module Rulers
     gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
     # “a7D” into “a7_D” or “b4M” into “b4_M”
     gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    #  turns all dashes into underscores
     tr("-", "_").
     downcase
   end
