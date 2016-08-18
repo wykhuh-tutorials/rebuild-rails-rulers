a57ae8ac6a45b9ee12119a840f2454cb37d49831
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-08-17 23:17:54 -0700

    3 add ::to_underscore

---
+++ lib/rulers/util.rb
@@ -0,0 +1,13 @@
+module Rulers
+  # convert CamelCase to snake_case
+  def self.to_underscore(string)
+    # replace '::' with '/'
+    string.gsub(/::/, '/').
+    # “BOBSays” to “BOB_says”
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    # “a7D” into “a7_D” or “b4M” into “b4_M”
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-", "_").
+    downcase
+  end
+end
