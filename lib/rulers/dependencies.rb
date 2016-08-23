94dca9e4fb6996c1892728ef9795e0f803cb558e
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-08-22 20:32:53 -0700

    3 add notes

--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -1,6 +1,13 @@
 class Object
+  # when an unknown constant (e.g class) is called, ruby calls const_missing
+
+  # const_missing + const_get lets Rulers & Rails to autoload files for a given
+  # class without having to write a 'require xxx' for each file
   def self.const_missing(c)
+    # turn QuotesController into 'quotes_controller'
+    # try to require/load a file  'quotes_controller'
     require Rulers.to_underscore(c.to_s)
+    # get the class QuotesController for symbol QuotesController
     Object.const_get(c)
   end
 end
--- lib/rulers/routing.rb
+++ lib/rulers/routing.rb
@@ -1,10 +1,12 @@
 module Rulers
   class Application
+    # convert /quotes/index to [QuotesController, index]
     def get_controller_and_action(env)
       # split no more than 4 times
       _, cont, action, after = env["PATH_INFO"].split('/', 4)
       cont = "#{cont.capitalize}Controller"
-      # const_get - look up any name starting with a capital letter
+      # const_get - look up any name starting with a capital letter.
+      # get the constant (eg class name)  for the given string
       [Object.const_get(cont), action]
     end
   end
