40120a819d4820c54259078c901aaebcf33842c2
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-08-22 20:46:03 -0700

    3 add check to prevent crash when controller does not exist

--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -4,10 +4,19 @@ class Object
   # const_missing + const_get lets Rulers & Rails to autoload files for a given
   # class without having to write a 'require xxx' for each file
   def self.const_missing(c)
+    # if QuotesController doesn't exist, the app goes in endless loop of
+    # const_missing and const_get. to prevent the endless loop, we do early
+    # return if const_missing is being called
+    return nil if @calling_const_missing
+
+    @calling_const_missing = true
     # turn QuotesController into 'quotes_controller'
     # try to require/load a file  'quotes_controller'
     require Rulers.to_underscore(c.to_s)
     # get the class QuotesController for symbol QuotesController
-    Object.const_get(c)
+    klass = Object.const_get(c)
+    @calling_const_missing = true
+
+    klass
   end
 end
