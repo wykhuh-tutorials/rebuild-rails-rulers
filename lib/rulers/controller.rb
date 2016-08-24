adaaf75a2091a39e10f6c4912e44fd43284e4a28
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-08-23 22:36:26 -0700

    4 add controller name to template path

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -10,10 +10,17 @@ module Rulers
 
     # locals is a hash of variables we pass to render
     def render(view_name, locals = {})
-      filepath = File.join "app", "views", "#{view_name}.html.erb"
+      filepath = File.join "app", "views", controller_name, "#{view_name}.html.erb"
       template = File.read filepath
       eruby = Erubis::Eruby.new(template)
       eruby.result(locals.merge(env: env))
+    end
+
+    # Convert QuotesController to quotes
+    def controller_name
+      klass = self.class
+      klass = klass.to_s.gsub /Controller$/, ""
+      Rulers.to_underscore(klass)
     end
   end
 end
